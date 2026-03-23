5e510f9a9accb9802bca7ab1bda06871ffc0e938
Dpbm <dpbm316@gmail.com>
2026-03-22 22:16:24 -0300

    refactoring

--- app/src/main/java/com/example/testble/MainActivity.kt
+++ app/src/main/java/com/example/testble/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testble
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.os.Bundle
@@ -22,26 +23,84 @@ import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.os.Build
 import android.util.Log
 import android.widget.Toast;
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TextButton
-import androidx.core.app.ActivityCompat
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
+import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
+import androidx.core.os.persistableBundleOf
+
+
+typealias Devices = HashMap<String,String>;
+
+
+object AppColors{
+    val green = Color(green=103, red=47, blue=63);
+    val white = Color(green=255, red=255, blue=255);
+    val red = Color(red=215, green=68, blue=86);
+    val gray = Color(191,201,209);
+    val purple = Color(red=155,green=142,blue=199);
+}
+
+class BLEManager{
+    companion object{
+        fun hasBLE(context: Context):Boolean{
+            return context.packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
+        }
+    }
+}
+
+class PermissionsManager(val context: Context){
+
+    @SuppressLint("InlinedApi")
+    val allPermissions = arrayOf(
+        Manifest.permission.BLUETOOTH_CONNECT,
+        Manifest.permission.BLUETOOTH_SCAN,
+        Manifest.permission.ACCESS_FINE_LOCATION);
+
+    fun remainingPermissions():List<String>{
+        return allPermissions.filter { p -> ContextCompat.checkSelfPermission(context, p) != PackageManager.PERMISSION_GRANTED };
+    }
+}
 
 class MainActivity : ComponentActivity() {
+    private val pm = PermissionsManager(this);
+    private val pmRequest = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+        values -> if(!values.all { it.value }){
+           makeToast("Failed on grant permissions!");
+        }else{
+            this.recreate();
+        }
+    }
 
+    private fun makeToast(text:String){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    /*
     private var btAdapter: BluetoothAdapter? = null;
     private var scanner: BluetoothLeScanner? = null;
 
+    private val devices: Devices = HashMap<String,String>();
 
+    @SuppressLint("MissingPermission")
     private val enableBtLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -72,6 +131,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    // For OLD BLUETOOTH
     private val gattCallback = object : BluetoothGattCallback(){
 
     }
@@ -104,6 +164,7 @@ class MainActivity : ComponentActivity() {
     }
 
     /*
+    //For old bluetooth
     private val receiver = object : BroadcastReceiver() {
         @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
         override fun onReceive(context: Context, intent: Intent) {
@@ -127,33 +188,8 @@ class MainActivity : ComponentActivity() {
         }
 
     }*/
-
-    private fun testHasBLE():Boolean{
-        return packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
     }
 
-    private fun checkBLPermissions():Boolean{
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-
-            val permissions = arrayOf(
-                Manifest.permission.BLUETOOTH_CONNECT,
-                Manifest.permission.BLUETOOTH_SCAN,
-                Manifest.permission.ACCESS_FINE_LOCATION);
-
-            val missing = permissions.filter{ permission -> ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED }
-
-            if(missing.isNotEmpty()) {
-                requestPermissions(missing.toTypedArray(), 100);
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private fun makeToast(text:String){
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
-    }
 
     @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
     fun listLocalDevices(){
@@ -178,30 +214,89 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
+     */
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column() {
-                HasBLE(
-                    has = testHasBLE()
-                )
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text("Test BLE") },
+                        colors = TopAppBarColors(
+                            containerColor = AppColors.green,
+                            scrolledContainerColor = AppColors.green,
+                            navigationIconContentColor = AppColors.green,
+                            titleContentColor = AppColors.white,
+                            actionIconContentColor = AppColors.green
+                        )
+                    )
+                },
+                content = { p ->
+                    Column(modifier = Modifier.padding(p)) {
+                        HasBLE(
+                            has = BLEManager.hasBLE(LocalContext.current)
+                        )
 
-                TextButton(onClick =  { listLocalDevices() }) {
-                    Text("List Devices")
+                        Permissions(pm.remainingPermissions()) {
+                            pmRequest.launch(
+                                pm.remainingPermissions().toTypedArray()
+                            )
+                        }
 
+                        /*TextButton(onClick =  { listLocalDevices() }) {
+                            Text("List Devices")
+                        }*/
+                    }
                 }
 
 
-            }
+            )
         }
     }
 }
 
+
 @Composable
 fun HasBLE(has: Boolean) {
     Text(
-        text = if(has) "Has BLE" else "Has not BLE feature!",
-        modifier = Modifier.padding(paddingValues = PaddingValues(all= Dp(20.0f)))
+        text = "This Device has " + if(!has) "not " else "" +  "access to BLE!",
+        modifier = Modifier.padding(paddingValues = PaddingValues(all= Dp(20.0f))),
+            fontSize = TextUnit(20.0f, type = TextUnitType.Sp),
+            color = if(has) AppColors.green else AppColors.red
     )
+}
+
+@Composable
+fun Permissions(permissions: List<String>, callback:(() -> Unit)){
+    Column(modifier = Modifier.padding(paddingValues = PaddingValues(top = Dp(20.0f), start = Dp(20.0f), end=Dp(20.0f)))) {
+        Text(
+            text = "Not Granted Permissions:",
+            fontSize = TextUnit(15.0f, type = TextUnitType.Sp),
+        )
+
+        LazyColumn {
+            items(permissions.size){ i ->
+                Text(permissions[i], color = AppColors.red)
+            }
+        }
+
+        TextButton(
+            onClick = { callback(); },
+            colors= ButtonColors(
+                containerColor = AppColors.purple,
+                contentColor =   AppColors.white,
+                disabledContainerColor = AppColors.gray,
+                disabledContentColor= AppColors.white
+            ),
+                enabled = permissions.isNotEmpty()
+        ) {
+            Text("Grant")
+        }
+
+
+    }
 }
